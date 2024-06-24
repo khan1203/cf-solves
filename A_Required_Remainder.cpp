8c88e836ea6fc0478b5d38248b3e890152b93467
khan1203 <rk.rifat1203@gmail.com>
2024-06-24 15:49:11 +0600

    Required Remainder solved

---
+++ A_Required_Remainder.cpp
@@ -0,0 +1,32 @@
+//by Khan Muhammad Rifat (CSE, BUBT)
+#include <bits/stdc++.h>
+#define ll long long
+#define nl endl
+#define PI 2*acos(0.0)
+using namespace std;
+
+void Fast_io(){
+    ios_base::sync_with_stdio(false);
+    cout.tie(0);
+    cin.tie(0);
+}
+
+void solve(){
+    // Fast_io();
+    ll x,y,n;
+    cin>>x>>y>>n;
+
+    ll k = (n-y)/x;
+    ll f = k*x + y;
+    cout<<f<<nl;
+}
+
+int main(){
+
+    // solve();
+
+    int t;
+    cin>>t;
+    while(t--){ solve(); }
+return 0;
+}
