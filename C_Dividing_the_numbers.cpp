1f4a5f0f685c9fd530b6b85d9ba82226e7edc08e
khan1203 <rk.rifat1203@gmail.com>
2024-10-17 23:37:05 +0600

    one more

---
+++ C_Dividing_the_numbers.cpp
@@ -0,0 +1,24 @@
+// by Khan Muhammad Rifat (CSE, BUBT)
+#include <bits/stdc++.h>
+#define ll long long
+#define nl endl
+#define PI 2 * acos(0.0)
+using namespace std;
+
+void Fast_io()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+}
+
+int main()
+{
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        }
+    return 0;
+}
